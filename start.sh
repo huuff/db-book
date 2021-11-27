7958cf59cb9030d6b27f3484a92a36eb93942e65
Haf <haf@protonmail.ch>
2021-11-27 23:29:36 +0100

    informative echos

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+echo "Destroying previous container..."
 sudo nixos-container destroy db-book
+
+echo "Creating a new container..."
 sudo nixos-container create db-book --flake .
+
+echo "Starting the container..."
 sudo nixos-container start db-book
+
+echo "Logging into it as root..."
 sudo nixos-container root-login db-book
