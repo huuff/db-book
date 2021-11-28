e43bd40ab19b71500eaf03767ef808ea1ed8b4ac
Haf <haf@protonmail.ch>
2021-11-28 17:41:32 +0100

    did 3.8

---
+++ exercises/chapter3/bank.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION ex38a()
+RETURNS TABLE ( id VARCHAR(5))
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  RETURN QUERY (
+    SELECT customer.id
+    FROM customer
+    WHERE NOT EXISTS (
+      SELECT *
+      FROM borrower
+      WHERE borrower.id = customer.id
+    )
+  );
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION ex38b(searching_id VARCHAR(5))
+RETURNS TABLE ( id VARCHAR(5) )
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+  searching_street VARCHAR(25) := (SELECT customer.customer_street FROM customer WHERE customer.id = searching_id);
+  searching_city VARCHAR(25) := (SELECT customer.customer_city FROM customer WHERE customer.id = searching_id);
+BEGIN
+  RETURN QUERY (
+    SELECT customer.id
+    FROM customer
+    WHERE customer.customer_street = searching_street AND customer.customer_city = searching_city
+  );
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION ex38c(searching_city VARCHAR(25))
+RETURNS TABLE ( branch_name VARCHAR(25))
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  RETURN QUERY (
+    SELECT DISTINCT(branch.branch_name)
+    FROM branch
+    JOIN account ON account.branch_name = branch.branch_name
+    JOIN depositor ON depositor.account_number = account.account_number
+    JOIN customer ON depositor.id = customer.id
+    WHERE customer_city = searching_city
+  );
+END;
+$$;
