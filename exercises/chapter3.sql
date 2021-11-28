4661a971a1312b7469b95f54d428791eb22ec440
Haf <haf@protonmail.ch>
2021-11-28 12:48:28 +0100

    added grade_points relation

--- exercises/chapter3.sql
+++ exercises/chapter3.sql
@@ -113,3 +113,27 @@ BEGIN
   );
 END;
 $$;
+
+CREATE TABLE IF NOT EXISTS grade_points(
+  grade VARCHAR(2) NOT NULL,
+  points NUMERIC(3, 1) NOT NULL,
+
+  PRIMARY KEY (grade)
+);
+
+INSERT INTO grade_points (grade, points)
+  VALUES
+    ('A+', 4.0),
+    ('A', 4.0),
+    ('A-', 3.7),
+    ('B+', 3.3),
+    ('B', 3.0),
+    ('B-', 2.7),
+    ('C+', 2.3),
+    ('C', 2.0),
+    ('C-', 1.7),
+    ('D+', 1.3),
+    ('D', 1.0),
+    ('E', 0.0),
+    ('F', 0.0)
+ON CONFLICT (grade) DO NOTHING;
