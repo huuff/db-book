a466b81be7d229802c46a6c9410eb86d291b91b6
Haf <haf@protonmail.ch>
2021-11-28 18:00:43 +0100

    did 3.9.a

---
+++ exercises/chapter3/employees.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION ex39a(search_company_name VARCHAR(25))
+RETURNS TABLE (
+  id INTEGER,
+  person_name VARCHAR(25),
+  city VARCHAR(25)
+)
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  RETURN QUERY (
+    SELECT employee.id, employee.person_name, employee.city
+    FROM employee
+    JOIN works ON works.id = employee.id
+    JOIN company ON company.company_name = works.company_name
+    WHERE company.company_name = search_company_name
+  );
+END;
+$$;
